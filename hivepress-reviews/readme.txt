3c88581f9fda6b4239e81466913b8bfff12d4e0b
Sean Murphy <sean@iamseanmurphy.com>
2024-12-28 16:17:31 +0000

    1.3.1

--- hivepress-reviews/readme.txt
+++ hivepress-reviews/readme.txt
@@ -2,9 +2,9 @@
 Contributors: hivepress
 Tags: hivepress, reviews, directory, listings, classifieds
 Requires at least: 5.0
-Tested up to: 6.6
-Requires PHP: 7.0
-Stable tag: 1.3.0
+Tested up to: 6.7
+Requires PHP: 7.4
+Stable tag: 1.3.1
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
@@ -14,7 +14,7 @@ Allow users to rate and review listings.
 
 HivePress Reviews is an extension for [HivePress](https://wordpress.org/plugins/hivepress/) plugin. It allows users to rate and review listings.
 
-[Demo](https://listinghive.hivepress.io/) | [Docs](https://help.hivepress.io/category/40-reviews) | [Support](https://community.hivepress.io/)
+[Demo](https://listinghive.hivepress.io/) | [Docs](https://help.hivepress.io/) | [Support](https://community.hivepress.io/)
 
 == Changelog ==
 
--- hivepress-reviews/vendor/composer/installed.php
+++ hivepress-reviews/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.3.0.1',
-        'version' => '1.3.0.1',
-        'reference' => 'c4565a396a5bb8cc2cb2ab7f538679260566ee05',
+        'pretty_version' => '1.3.1.1',
+        'version' => '1.3.1.1',
+        'reference' => '37100cd51f1ff3cdf745dc6255a74c1776a7cffa',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.3.0.1',
-            'version' => '1.3.0.1',
-            'reference' => 'c4565a396a5bb8cc2cb2ab7f538679260566ee05',
+            'pretty_version' => '1.3.1.1',
+            'version' => '1.3.1.1',
+            'reference' => '37100cd51f1ff3cdf745dc6255a74c1776a7cffa',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
